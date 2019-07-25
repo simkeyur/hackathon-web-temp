f5a4d5d30b4c63d7fc55aaf04ba28f2845bc288d
Keyur Patel <simkeyur@gmail.com>
2019-07-25 09:25:01 -0500

    Added Mockaroo API for UI testing

--- scripts/AngularControllers/judging.js
+++ scripts/AngularControllers/judging.js
@@ -1,10 +1,14 @@
 var app = angular.module('SHP-Web', []);
 app.controller('JudgingCtrl', function($scope, $http) {
-	$scope.Title = "Judging";
+	$scope.Title = "Our Judging Panel";
 
     $scope.loaded = false;
+    $http.get('https://my.api.mockaroo.com/judges.json?key=3e684640').
+        then(function(response) {
+            $scope.Judges = response.data;
+            //When Data is loaded
+            $scope.loaded = true;
+        });
     
-    //When Data is loaded
-    $scope.loaded = true;
    
 });
